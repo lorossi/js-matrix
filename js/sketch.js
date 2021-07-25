d77c6e8cce018065b119a0a5140e1001f99c6cb2
lorossi <contact@lorenzoros.si>
2021-07-26 01:30:14 +0200

    code cleaned up a little, added recording capabilities

--- js/sketch.js
+++ js/sketch.js
@@ -2,15 +2,28 @@ class Sketch extends Engine {
   preload() {
     this._chains_num = 150; // number of falling chains of letters
     this._duration = 600; // animation duration
-    this._border = 100;
+    this._recording = false;
   }
 
   setup() {
+    // create chains
     this._chains = [];
     for (let i = 0; i < this._chains_num; i++) this._chains.push(new Chain(this.height, this._duration));
+    // setup capturer
+    if (this._recording) {
+      this._capturer = new CCapture({ format: "png" });
+      this._capturer_started = false;
+    }
   }
 
   draw() {
+    // start capturer
+    if (!this._capturer_started && this._recording) {
+      this._capturer_started = true;
+      this._capturer.start();
+      console.log("%c Recording started", "color: green; font-size: 2rem");
+    }
+
     const current_frame = this.frameCount % 2 == 0 ? 0 : this._duration;
     this.ctx.save();
     this.background("#151515");
@@ -22,5 +35,17 @@ class Sketch extends Engine {
       c.show(this.ctx);
     });
     this.ctx.restore();
+
+    // save frame
+    if (this._recording) {
+      if (this.frameCount < this._duration) {
+        this._capturer.capture(this._canvas);
+      } else {
+        this._recording = false;
+        this._capturer.stop();
+        this._capturer.save();
+        console.log("%c Recording ended", "color: red; font-size: 2rem");
+      }
+    }
   }
 }
