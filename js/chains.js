c00c9ec485ff072f84c5633a42de8ee39d1dd1a9
lorossi <contact@lorenzoros.si>
2021-07-26 01:24:21 +0200

    better looping around

--- js/chains.js
+++ js/chains.js
@@ -1,9 +1,8 @@
 class Chain {
-  constructor(canvas_width, canvas_height, max_duration) {
-    this._canvas_width = canvas_width;
-    this._canvas_height = canvas_height;
-    this._max_duration = max_duration;
-    this._max_statuses = 10;
+  constructor(canvas_size, total_duration) {
+    this._canvas_size = canvas_size;
+    this._total_duration = total_duration;
+    this._total_statuses = 10;
     this._reset();
     this._generateStatuses();
   }
@@ -13,22 +12,23 @@ class Chain {
     this._length = 4 + Math.floor(Math.random() * 8);
     // "time" offset, so not every letter changes at the same time
     this._offset = Math.random();
-
-    this._duration = this._max_duration / Math.floor(Math.random() * 3 + 1);
-    this._scl = Math.random() * 20 + 10; // letter size
-
+    // fall duration relative to total duration
+    this._duration = this._total_duration / Math.floor(Math.random() * 3 + 1);
+    // letter size
+    this._scl = Math.random() * 20 + 10;
     // random x and y starting positions
-    this._x = Math.random() * this._canvas_width;
-    this._start_y = Math.random() * this._canvas_height;
+    this._x = Math.random() * this._canvas_size; // x coordinate never changes
+    this._start_y = Math.random() * this._canvas_size;
     // period of the status change
-    this._status_period = this._max_duration / this._max_statuses;
+    this._status_period = this._total_duration / this._total_statuses;
     // current status
     this._status_counter = 0;
   }
 
+  // each status is a set of letters
   _generateStatuses() {
     this._statuses = [];
-    for (let i = 0; i < this._max_statuses; i++) {
+    for (let i = 0; i < this._total_statuses; i++) {
       this._statuses.push(new Array(this._length));
       for (let j = 0; j < this._length; j++) {
         this._statuses[i][j] = String.fromCodePoint(0x3041 + Math.floor(Math.random() * 63));
@@ -39,10 +39,10 @@ class Chain {
   move(frame_count) {
     const percent = (frame_count % this._duration) / this._duration;
     // move down the chain
-    this._current_y = this._start_y + this._canvas_height * percent;
-    while (this._current_y > this._canvas_height) this._current_y -= this._canvas_height;
+    this._current_y = this._start_y + (this._canvas_size + this._scl * this._length) * percent;
+    while (this._current_y > this._canvas_size) this._current_y -= this._canvas_size + this._scl * this._length;
     // select current status
-    this._status_counter = Math.floor(percent * this._max_statuses + this._offset) % this._max_statuses;
+    this._status_counter = Math.floor(percent * this._total_statuses + this._offset) % this._total_statuses;
   }
 
   show(ctx) {
--- js/sketch.js
+++ js/sketch.js
@@ -1,18 +1,13 @@
 class Sketch extends Engine {
   preload() {
-    this._chains_num = 1; // number of falling chains of letters
+    this._chains_num = 100; // number of falling chains of letters
     this._duration = 600; // animation duration
     this._border = 100;
-
-    this._juggle = false;
   }
 
   setup() {
     this._chains = [];
-    for (let i = 0; i < this._chains_num; i++) {
-      const nc = new Chain(this.width, this.height, this._duration);
-      this._chains.push(nc);
-    }
+    for (let i = 0; i < this._chains_num; i++) this._chains.push(new Chain(this.height, this._duration));
   }
 
   draw() {
@@ -27,7 +22,5 @@ class Sketch extends Engine {
       c.show(this.ctx);
     });
     this.ctx.restore();
-
-    //if (this.frameCount == 0 || this.frameCount == this._duration - 1) this.saveFrame();
   }
 }
